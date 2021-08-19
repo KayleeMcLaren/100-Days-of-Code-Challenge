b013229569728efb60ac9e9b5d35cdeeac8ac832
KayleeMcLaren <69905377+KayleeMcLaren@users.noreply.github.com>
2021-08-19 09:11:17 +0200

    Create hurdle 4.py
    This code is for the Reeborg's World Hurdle 4. The object of the game is to write code which enables the Robot to clear the hurdles and travel to the finish line. Every time the game starts, the position and height of the hurdles changes randomly so the code has to work no matter what the hurdles look like. The link to the game can be found here: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json

---
+++ Day 6 - Escape The Maze/hurdle 4.py
@@ -0,0 +1,16 @@
+# code for Reeborg's World Hurdle 4 game
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()        
+    else:
+        turn_left()
+        
