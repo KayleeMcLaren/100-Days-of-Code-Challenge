0066f3fb17d66d8d30a431488423d0af75f89c63
KayleeMcLaren <69905377+KayleeMcLaren@users.noreply.github.com>
2021-08-26 08:15:25 +0200

    Create debugging_leap_year.py
    Debugging error in Leap Year Checker program.

---
+++ Day 13 - Debugging Practice/debugging_leap_year.py
@@ -0,0 +1,31 @@
+############DEBUGGING#####################
+
+# Debug Leap Year Checker:
+
+year = input("Which year do you want to check?")
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+
+
+# Solution:
+
+year = int(input("Which year do you want to check?")) # in order to check the year, the input should be cast to an integer
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+  
