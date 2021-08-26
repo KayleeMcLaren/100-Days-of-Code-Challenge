a8d2b5d979fb656bf1c59bb9b829acac2d01164a
KayleeMcLaren <69905377+KayleeMcLaren@users.noreply.github.com>
2021-08-26 08:12:59 +0200

    Create debugging_odd_or_even.py
    Debugging error in Odd or Even program.

---
+++ Day 13 - Debuging Practice/debugging_odd_or_even.py
@@ -0,0 +1,22 @@
+############DEBUGGING#####################
+
+# Debug Odd or Even:
+
+number = int(input("Which number do you want to check?"))
+
+if number % 2 = 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
+
+
+# Solution:
+
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0: # one equals sign is assignment, while two equals signs check for equality
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
+    
+    
