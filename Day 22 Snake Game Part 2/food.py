954ce2e607ac2660a91bc5e1692c87b930d9ab6e
KayleeMcLaren <69905377+KayleeMcLaren@users.noreply.github.com>
2021-09-04 16:43:14 +0200

    Create food.py
    The Food class used in the main.py file.

---
+++ Day 22 Snake Game Part 2/food.py
@@ -0,0 +1,23 @@
+
+from turtle import Turtle
+import random
+
+"""The Food class creates the food object and has a function called refresh which, after the snake object collides with the food object, 
+a new food object appears on screen at a random position."""
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("purple")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
+        
+        
