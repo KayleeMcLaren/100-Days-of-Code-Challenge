7ecbdc9dc5fd357f22dab7b5a3387905dddccb9b
KayleeMcLaren <69905377+KayleeMcLaren@users.noreply.github.com>
2021-09-07 09:26:37 +0200

    Update scoreboard.py
    The Scoreboard class used in the main.py file.

--- Day 23 - Turtle Crossing/scoreboard.py
+++ Day 23 - Turtle Crossing/scoreboard.py
@@ -5,6 +5,8 @@ FONT = ("Courier", 24, "normal")
 
 
 class Scoreboard(Turtle):
+    """The Scoreborad class creates the scoreboard for the game. It update_scorebaord method updates the scoreboard adn is called after each successful round. 
+    The increase_level method increases the score. And the game_over method is called when the turtle character collides with a car, and displays the appropriate message"""
     def __init__(self):
         super().__init__()
         self.penup()
