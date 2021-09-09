6a53585580cf0c74a653b0f3499a253d5f28f455
KayleeMcLaren <69905377+KayleeMcLaren@users.noreply.github.com>
2021-09-09 15:08:08 +0200

    Update scoreboard.py

--- Day 21 - Snake Game Part 2/scoreboard.py
+++ Day 21 - Snake Game Part 2/scoreboard.py
@@ -3,12 +3,17 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Rockwell", 24, "normal")
 
-"""The Scoreboard class is used to display a scoreboard on the screen using the update_screen function. 
-It increases the score with the increase_score fucntion and displays Game Over with the game_over fucntion."""
+
 class Scoreboard(Turtle):
+    """The Scoreboard class is used to display a scoreboard on the screen using the update_screen function. 
+    It increases the score with the increase_score function. The reset function check's if the user has achived
+    a new high score and if so, writes that score to the data.txt file to save that high score for the next time
+    the game is played."""
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -16,14 +21,18 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score}   High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        with open("data.txt", mode="w") as data:
+            data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
         
