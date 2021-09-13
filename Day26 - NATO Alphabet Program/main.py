ace433f01f4e993dd410fa46951e6f904171511d
KayleeMcLaren <69905377+KayleeMcLaren@users.noreply.github.com>
2021-09-13 12:31:06 +0200

    Update main.py

--- Day26 - NATO Alphabet Program/main.py
+++ Day26 - NATO Alphabet Program/main.py
@@ -6,8 +6,18 @@ code_df = pandas.DataFrame(code_dict)
 
 alphabet_dict = {row.letter: row.code for (index, row) in code_df.iterrows()}
 
-user_word = input("Enter a word: ").upper()
 
-code_words = [alphabet_dict[letter] for letter in user_word]
+def generate_phonetic():
+    user_word = input("Enter a word: ").upper()
+    try:
+        code_words = [alphabet_dict[letter] for letter in user_word]
 
-print(code_words)
+    except KeyError:
+        print("\nSorry, only letters in the alphabet please.")
+        generate_phonetic()
+
+    else:
+        print(code_words)
+
+
+generate_phonetic()
