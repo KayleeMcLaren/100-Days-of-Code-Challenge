faa9731de0e915b660d7efd94527a32d55d22388
KayleeMcLaren <69905377+KayleeMcLaren@users.noreply.github.com>
2021-09-17 16:02:39 +0200

    Update ui.py

--- Day34 - GUI Quiz App/ui.py
+++ Day34 - GUI Quiz App/ui.py
@@ -20,11 +20,11 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(150, 125, width=280, text="Question", font=("Arial", 20, "italic"), fill="black")
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
-        true_img = PhotoImage(file="images/true.png")
+        true_img = PhotoImage(file="true.png")
         self.true_button = Button(image=true_img, highlightthickness=0, command=self.clicked_true)
         self.true_button.grid(column=0, row=2)
 
-        false_img = PhotoImage(file="images/false.png")
+        false_img = PhotoImage(file="false.png")
         self.false_button = Button(image=false_img, highlightthickness=0, command=self.clicked_false)
         self.false_button.grid(column=1, row=2)
 
